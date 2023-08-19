0b15e9cd6c38ae5fba70942ba576f5aa44651975
Christoph Freier <christoph@freier.fr>
2023-08-19 09:41:11 +0200

    Initial Commit

---
+++ RingsCards/RingsCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingsCardsApp.swift
+//  RingsCards
+//
+//  Created by Christoph Freier on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct RingsCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
