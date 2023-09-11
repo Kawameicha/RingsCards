8930bf20c8adfbde3839c478b0ea5c086bc673c8
Christoph Freier <christoph@freier.fr>
2023-09-11 11:29:02 +0200

    Add BuilderAdd

---
+++ RingsCards/Views/BuilderViews/BuilderAdd.swift
@@ -0,0 +1,127 @@
+//
+//  BuilderAdd.swift
+//  RingsCards
+//
+//  Created by Christoph Freier on 11.09.23.
+//
+
+import SwiftUI
+
+struct BuilderAdd: View {
+    @EnvironmentObject var ringsData: RingsData
+    @State private var searchText: String = ""
+    @State private var sortBySphere = false
+    @State private var showFilters = false
+    @State private var showCollection = false
+
+    var deck: Deck
+    var value = 0
+
+    var collection: [String] {
+        ringsData.collection.map { pack in
+            pack.isInCollection ? pack.PackCode : ""
+        }
+    }
+
+    var collectionOnly: [Card] {
+        ringsData.cards.filter { card in
+            card.pack_code.within(collection)
+        }
+    }
+
+    var filtered: [String] {
+        ringsData.spheres.map { sphere in
+            sphere.filterIn ? sphere.name : ""
+        }
+    }
+
+    var filteredCards: [Card] {
+        collectionOnly.filter { card in
+            card.sphere_name.within(filtered)
+        }
+    }
+
+    var sortedCards: [Card] {
+        filteredCards.sorted(by: {
+            if sortBySphere == true && $0.sphere_code != $1.sphere_code {
+                return $0.sphere_code < $1.sphere_code
+            } else {
+                return $0.name < $1.name
+            }
+        })
+    }
+
+    var searchedCards: [Card] {
+        guard !searchText.isEmpty else { return sortedCards }
+        return sortedCards.filter { card in
+            card.name.lowercased().cleaned().contains(searchText.lowercased())
+        }
+    }
+
+    var allKeys: [String] { deck.slots.map{String($0.key) } }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(CardAnatomy.CardType.allCases.map { $0.rawValue.capitalized }, id:\.self) { type in
+                    Section(header: Text("Add \(type)")) {
+                        ForEach(searchedCards.filter { card in
+                            !card.code.within(allKeys) && card.type_name.contains("\(type)")
+                        }) {card in
+                            NavigationLink {
+                                CardView(card: card)
+                            } label: {
+                                BuilderRow(deck: deck, card: card)
+                            }
+                        }
+                    }
+                }
+            }
+            .listStyle(.sidebar)
+            .navigationTitle("Add Cards")
+            .searchable(text: $searchText)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu(content: {
+                        Button {
+                            sortBySphere.toggle()
+                        } label: {
+                            Label("Sort by Sphere", systemImage: "arrow.up.arrow.down")
+                        }
+                        Button {
+                            showFilters.toggle()
+                        } label: {
+                            Label("Show Filters", systemImage: "line.3.horizontal.decrease")
+                        }
+                        Button {
+                            showCollection.toggle()
+                        } label: {
+                            Label("Edit Collection", systemImage: "checklist")
+                        }
+                    }) {
+                        Image(systemName: "ellipsis.circle")
+                    }
+                }
+            }
+            .sheet(isPresented: $showCollection) {
+                CollectionView()
+                    .environmentObject(ringsData)
+                    .presentationDetents([.medium, .large])
+            }
+            .sheet(isPresented: $showFilters) {
+                FilterEdit()
+                    .environmentObject(ringsData)
+                    .presentationDetents([.medium, .large])
+            }
+        }
+    }
+}
+
+struct BuilderAdd_Previews: PreviewProvider {
+    static var decks = RingsData().decks
+
+    static var previews: some View {
+        BuilderAdd(deck: decks[0])
+            .environmentObject(RingsData())
+    }
+}
--- RingsCards/Views/DeckViews/DeckView.swift
+++ RingsCards/Views/DeckViews/DeckView.swift
@@ -10,20 +10,23 @@ import SwiftUI
 struct DeckView: View {
     @EnvironmentObject var ringsData: RingsData
     @State private var editDeck = false
+    @State private var addCards = false
 
     var deck: Deck
 
     // Merge new cards into slots
-    var campaignSlots: [String:Int] {
-        deck.slots.merging(["01132": 1]) { (_, new) in new }
-    }
+//    var campaignSlots: [String:Int] {
+//        deck.slots.merging(["01132": 1]) { (_, new) in new }
+//    }
+
+    var allKeys: [String] { deck.slots.map{String($0.key) } }
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(CardAnatomy.CardType.allCases.map { $0.rawValue.capitalized }, id:\.self) { type in
                     Section(header: Text("\(type)")) {
-                        ForEach(campaignSlots.sorted(by: >), id: \.key) { key, value in
+                        ForEach(deck.slots.sorted(by: >), id: \.key) { key, value in
                             ForEach(ringsData.cards.filter { card in
                                 card.code.contains("\(key)") && card.type_name.contains("\(type)")
                             }) {card in
@@ -69,10 +72,20 @@ struct DeckView: View {
                         } label: {
                             Label("Edit Deck", systemImage: "plus.forwardslash.minus")
                         }
+                        Button {
+                            addCards.toggle()
+                        } label: {
+                            Label("Add Card", systemImage: "plus.rectangle.portrait")
+                        }
                     }) {
                         Image(systemName: "ellipsis.circle")
                     }
                 }
+            }
+            .sheet(isPresented: $addCards) {
+                BuilderAdd(deck: deck)
+                    .environmentObject(ringsData)
+                    .presentationDetents([.medium, .large])
             }
         }
     }
