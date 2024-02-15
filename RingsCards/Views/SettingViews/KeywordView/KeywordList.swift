95a1c75f9773797fad8dda56f7b264d89ee7468d
Willy Breitenbach <willybr@me.com>
2024-02-15 19:42:57 +0100

    merge conflict

--- RingsCards/Views/SettingViews/KeywordView/KeywordList.swift
+++ RingsCards/Views/SettingViews/KeywordView/KeywordList.swift
@@ -11,18 +11,27 @@ import SwiftData
 struct KeywordList: View {
     @Environment(ViewRuleModel.self) var viewRuleModel
     @Environment(\.modelContext) var modelContext
-    @Query var keywords: [Rule]
 
+    var body: some View {
+        @Bindable var viewRuleModel = viewRuleModel
+        
+        KeywordListView(searchText: viewRuleModel.searchText)
+            .navigationTitle("Keywords")
+            .searchable(text: $viewRuleModel.searchText)
+    }
+}
+
+private struct KeywordListView: View {
+    @Query var keywords: [Rule]
+    
     init(
         searchText: String = ""
     ) {
         _keywords = Query(filter: #Predicate<Rule> { rule in
             rule.isKeyword && (searchText.isEmpty || rule.name.localizedStandardContains(searchText))}, sort: \Rule.id)
     }
-
+    
     var body: some View {
-        @Bindable var viewRuleModel = viewRuleModel
-
         List {
             ForEach(keywords) { keyword in
                 NavigationLink(value: Router.ruleView(keyword)) {
@@ -30,8 +39,6 @@ struct KeywordList: View {
                 }
             }
         }
-        .navigationTitle("Keywords")
-        .searchable(text: $viewRuleModel.searchText)
     }
 }
 
--- RingsCards/Views/SettingViews/RuleView/RuleList.swift
+++ RingsCards/Views/SettingViews/RuleView/RuleList.swift
@@ -12,18 +12,10 @@ struct RuleList: View {
     @Environment(ViewRuleModel.self) var viewRuleModel
     @Environment(\.modelContext) var modelContext
 
-    private var filterRule: [String]
-    
-    init(
-        filterRule: [String] = []
-    ) {
-        self.filterRule = filterRule
-    }
-
     var body: some View {
         @Bindable var viewRuleModel = viewRuleModel
         
-        RuleListView(filterRule: filterRule, searchText: viewRuleModel.searchText)
+        RuleListView(searchText: viewRuleModel.searchText)
             .navigationTitle("Glossary")
             .searchable(text: $viewRuleModel.searchText)
     }
@@ -33,14 +25,14 @@ private struct RuleListView: View {
     @Query var rules: [Rule]
     
     init(
-        filterRule: [String] = [],
         searchText: String
     ) {
+        _rules = Query(filter: #Predicate<Rule> { rule in
+            !rule.isKeyword && (searchText.isEmpty || rule.name.localizedStandardContains(searchText))}, sort: \Rule.id)
     }
     
     var body: some View {
-        _rules = Query(filter: #Predicate<Rule> { rule in
-            !rule.isKeyword && (searchText.isEmpty || rule.name.localizedStandardContains(searchText))}, sort: \Rule.id)
+        
         List {
             ForEach(rules) { rule in
                 NavigationLink(value: Router.ruleView(rule)) {
