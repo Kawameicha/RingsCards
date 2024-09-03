265541ac4a3bbfe5a4f4ae28c08c0e6a7a4e6726
Christoph Freier <christoph@freier.fr>
2024-09-03 14:22:39 +0200

    Refactor DeckRow

--- RingsCards/Views/DeckViews/DeckList/DeckRow.swift
+++ RingsCards/Views/DeckViews/DeckList/DeckRow.swift
@@ -16,33 +16,28 @@ struct DeckRow: View {
         let heroes = deck.heroes.reduce(0) { (result, keyValue) in
             return result + keyValue.value }
 
-        HStack(spacing: 3) {
+        HStack(alignment: .center, spacing: 3) {
             Image(systemName: "\(deck.threat).square")
-                .foregroundStyle(.white)
                 .font(.system(size: 42))
 
             VStack(alignment: .leading, spacing: 3) {
                 Text(deck.name)
-                    .foregroundStyle(.white)
                     .font(.headline)
 
                 HStack(alignment: .bottom, spacing: 3) {
                     Image(systemName: "person.3.fill")
-                        .font(.system(size: 14))
                     Text("x\(heroes)     ")
-                        .font(.subheadline)
                     Image(systemName: "rectangle.portrait.inset.filled")
-                        .font(.system(size: 14))
                     Text("x\(player - heroes)")
-                        .font(.subheadline)
                 }
-                .foregroundStyle(.white)
+                .font(.subheadline)
             }
 
             Spacer()
 
             DeckChart(deck: deck)
         }
+        .foregroundStyle(.white)
         .lineLimit(1)
     }
 }
