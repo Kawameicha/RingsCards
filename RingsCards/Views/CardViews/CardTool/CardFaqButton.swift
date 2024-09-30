1d6ed528753ebe7f9ea70a28a19c9b9579b39b00
Christoph Freier <christoph@freier.fr>
2024-09-30 15:03:01 +0200

    Update CardFaqButton

--- RingsCards/Views/CardViews/CardTool/CardFaqButton.swift
+++ RingsCards/Views/CardViews/CardTool/CardFaqButton.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct CardFaqButton: View {
     @Query var errata: [Erratum]
-    @State private var showAlert = false
+    @State private var showSheet = false
     var card: Card
 
     init(
@@ -25,26 +25,30 @@ struct CardFaqButton: View {
 
     var body: some View {
         if !errata.isEmpty {
-            let source = if errata.first?.isOfficial == true {
-                "official"
-            } else {
-                "ALeP"
-            }
+            let source = errata.first?.isOfficial == true ? "official" : "ALeP"
 
             Button(action: {
-                showAlert = true
+                showSheet = true
             }) {
                 Image(systemName: "info.circle")
             }
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("This card was subject to \(source) errata"),
-                    message: Text(.init("\(errata.first?.text ?? "")"
+            .sheet(isPresented: $showSheet) {
+                VStack(alignment: .center) {
+                    Text("This card was subject to \(source) errata")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+
+                    Text(.init("\(errata.first?.text ?? "")"
                         .iconsAndSpheres()
                     ))
-                    .font(Font.custom("SFUIText-Regular", size: 16)),
-                    dismissButton: .default(Text("OK"))
-                )
+                    .font(Font.custom("SFUIText-Regular", size: 16))
+                    .padding()
+
+                    Spacer()
+                }
+                .presentationDetents([.medium])
             }
         } else {
             EmptyView()
